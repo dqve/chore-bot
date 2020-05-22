3ff9472250b54954dffa9491ae2f43039ee9dca4
Ayoola David <ayooladavid@gmail.com>
2020-05-22 02:05:54 +0100

    update functions

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 let doorImages = document.querySelectorAll('.door-frame')
+let numClosedDoors = doorImages.length;
+let openDoorA, openDoorB, openDoorC;
+
 const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg"
 
 const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg"
 
 const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg"
 
-const imageList = [botDoorPath,beachDoorPath,spaceDoorPath]
+randomChoreDoorGenerator = ()=>{
+    const numClosedDoors = 3;
+    const choreDoor = Math.floor(Math.random() * numClosedDoors);
 
-for (let i = 0; i < doorImages.length; i++){
-  let anImage = doorImages[i]
-  let anImagePic = imageList[i]
-  anImage.onclick = () => (
-      anImage.src = imageList[i]
-      
-      )
-   
+    if(choreDoor === 0) {
+    openDoorA = botDoorPath;
+    openDoorB = beachDoorPath;
+    openDoorC = spaceDoorPath;
+  } else if (choreDoor === 1) {
+    openDoorB = botDoorPath;
+    openDoorC = beachDoorPath;
+    openDoorA = spaceDoorPath;
+  } else if (choreDoor === 2) {
+    openDoorC = botDoorPath;
+    openDoorA = beachDoorPath;
+    openDoorB = spaceDoorPath;
+  } 
+  console.log(choreDoor)
+	const imageList = [openDoorA,openDoorB,openDoorC]
+
+	for (let i = 0; i < doorImages.length; i++){
+	  let anImage = doorImages[i]
+	  let anImagePic = imageList[i]
+	  anImage.onclick = () => (
+	      anImage.src = imageList[i]
+	      )
+	   
+	}
 }
+
+
+randomChoreDoorGenerator()
